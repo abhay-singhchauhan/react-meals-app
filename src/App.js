da4fe0eafc7f2920a7f6be4342d3a89057e2135e
abhay singh chauhan <itsyourabhay@gmail.com>
2024-01-12 23:32:51 +0530

    added the overlay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Modal from "./components/Layout/Modal";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Modal></Modal>
       <h2>Let's get started!</h2>
     </div>
   );
---
+++ src/components/Layout/Modal.js
@@ -0,0 +1,18 @@
+import "./Modal.css";
+
+const Modal = () => {
+  return (
+    <div className="mainModal">
+      <h1>Delicious Food, Delivered to you</h1>
+      <p>
+        Choose your favrate meal from our broad selection of available meals and
+        enjoy a Delicious lunch or dinner at your home.{" "}
+      </p>
+      <p>
+        All our meals are cooked with high quality ingredients, just-in-time and
+        ofcurse by experianced chefs
+      </p>
+    </div>
+  );
+};
+export default Modal;
