588e5dda5a244a6f6761680acb89e459938be89a
abhay singh chauhan <itsyourabhay@gmail.com>
2024-01-21 19:57:34 +0530

    added the backdrop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Header from "./components/Layout/Header";
 import Modal from "./components/Layout/Modal";
 import MealList from "./components/Meals/Meallist";
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart></Cart>
       <Header></Header>
       <Modal></Modal>
       <MealList></MealList>
